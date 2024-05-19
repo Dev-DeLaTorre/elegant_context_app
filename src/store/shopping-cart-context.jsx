9867b2caad91586df033ce2193410f2ee9b06371
Luis De La Torre A <dev.delatorre.l@gmail.com>
2024-05-19 18:20:58 -0500

    applying euseReducer for complex state management

--- src/store/shopping-cart-context.jsx
+++ src/store/shopping-cart-context.jsx
@@ -1,7 +1,100 @@
-import { createContext } from "react";
+import { createContext, useState, useReducer } from "react";
+import { DUMMY_PRODUCTS } from "../dummy-products";
 
 export const CartContext = createContext({
     items: [],
     addItemToCart: () => {},
-    updateCartItem: () => {}
+    updateCartItem: () => {},
 });
+
+function ShoppingCartReducer(state, action){
+    if(action.type === "ADD_ITEM"){
+        const updatedItems = [...state.items];
+    
+          const existingCartItemIndex = updatedItems.findIndex(
+            (cartItem) => cartItem.id === action.payload.id
+          );
+          const existingCartItem = updatedItems[existingCartItemIndex];
+    
+          if (existingCartItem) {
+            const updatedItem = {
+              ...existingCartItem,
+              quantity: existingCartItem.quantity + 1,
+            };
+            updatedItems[existingCartItemIndex] = updatedItem;
+          } else {
+            const product = DUMMY_PRODUCTS.find((product) => product.id === action.payload.id);
+            updatedItems.push({
+              id: action.payload.id,
+              name: product.title,
+              price: product.price,
+              quantity: 1,
+            });
+          }
+    
+          return {
+            items: updatedItems,
+          };
+    }
+    if(action.type === "UPDATE_ITEM"){
+        const updatedItems = [...state.items];
+          const updatedItemIndex = updatedItems.findIndex(
+            (item) => item.id === action.payload.productId
+          );
+    
+          const updatedItem = {
+            ...updatedItems[updatedItemIndex],
+          };
+    
+          updatedItem.quantity += action.payload.amount;
+    
+          if (updatedItem.quantity <= 0) {
+            updatedItems.splice(updatedItemIndex, 1);
+          } else {
+            updatedItems[updatedItemIndex] = updatedItem;
+          }
+    
+          return {
+            items: updatedItems,
+          };
+    }
+    
+    return state;
+}
+
+export default function ContextProviderComponent({ children }){
+    /*const [shoppingCart, setShoppingCart] = useState({
+        items: [],
+      });*/
+    
+      const [ shoppingCart, shoppingCartDispatcher] = useReducer(ShoppingCartReducer, {
+        items: [],
+      });
+    
+      function handleAddItemToCart(id) {
+        shoppingCartDispatcher({
+            type: "ADD_ITEM",
+            payload: {
+                id,
+            }
+        });
+      }
+    
+      function handleUpdateCartItemQuantity(productId, amount) {
+        shoppingCartDispatcher({
+            type: "UPDATE_ITEM",
+            payload: {
+                productId,
+                amount,
+            }
+        });
+      }
+    
+      const contextValues = {items: shoppingCart.items,
+         addItemToCart: handleAddItemToCart,
+         updateCartItem: handleUpdateCartItemQuantity}
+
+    return <CartContext.Provider value={contextValues}>
+        {children}
+    </CartContext.Provider>;
+}
